9c59673a5ae889f54706cf8ae7bee13f884e25d4
DeegiiAlt <aagiideegii@gmail.com>
2018-02-16 17:59:43 -0700

    test

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 var inquirer = require("inquirer");
 
-var guessesLeft = 10;
+// var guessesLeft = 10;
 
-inquirer.prompt([
-	{
-		type: "input",
-		message: "? Guess a letter!",
-		name: "userGuess"
-	}
-])
-.then(function(answers) {
-	if(answers.userGuess === //array of letters of word to guess
-		) {
-		console.log("CORRECT!!!");
-		//push correct letter guessed to fill blank
-	} else {
-		console.log("INCORRECT!!!");
-		guessesLeft--;
-		console.log(`${guessesLeft} guesses remaining!!!`)
-	}
-})
+// inquirer.prompt([
+// 	{
+// 		type: "input",
+// 		message: "? Guess a letter!",
+// 		name: "userGuess"
+// 	}
+// ])
+// .then(function(answers) {
+// 	if(answers.userGuess === //array of letters of word to guess
+// 		) {
+// 		console.log("CORRECT!!!");
+// 		//push correct letter guessed to fill blank
+// 	} else {
+// 		console.log("INCORRECT!!!");
+// 		guessesLeft--;
+// 		console.log(`${guessesLeft} guesses remaining!!!`)
+// 	}
+// })
--- word.js
+++ word.js
@@ -1,5 +1,23 @@
 var Letter = require("./letter.js");
 
-var Word = function (letterArray) {
-	this.letterArray = new Letter([]);
+var Word = function (words) {
+	this.words = words;//takes in words from index.js
+	var wordArr = words.split(", ");//splits words and puts into array
+	var randomWord = wordArr[Math.floor(Math.random() * wordArr.length)];//chooses random word
+	var characters = randomWord.split("");
+	characters = new Letter(characters);
+	this.word = function() {
+		Letter.lettersGuessed();
+	}
 }
+
+//to run
+// characters.Letter();
+
+//would be in index passed through word.
+// var words = "spirited away, howls moving castle, princess mononoke";
+
+// console.log(randomWord);
+
+
+// console.log(letters);
