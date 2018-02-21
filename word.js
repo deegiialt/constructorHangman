f02257d9bbb80e310c0b3172960f867b6fa9e5a1
DeegiiAlt <aagiideegii@gmail.com>
2018-02-20 19:26:22 -0700

    test

--- word.js
+++ word.js
@@ -4,23 +4,33 @@ var Word = function (words) {
 	this.words = words;//takes in words from index.js
 	var wordArr = words.split(", ");//splits words and puts into array
 	var randomWord = wordArr[Math.floor(Math.random() * wordArr.length)];//chooses random word
-	var characters = randomWord.split("");
+	var characters = randomWord.split("").join(" ");// array of letters
 	//pushes characters into Letter constructor
 	var newChar = new Letter(characters);
-////////////////////////////////
 //loop through characters array and display _ same as length
+	// var matchedLetters = [];
+	var answerArray = [];
+	this.found = false;
+	this.guessesTogether = function() {
+		for(var i = 0; i < characters.length; i++) {
+			this.answerArray.push(newChar);
+			// newChar.lettersGuessed();
+			// answerArray[i] = " _ ";
+		}
+		// console.log(answerArray.join(" "))
+	}
+	this.find = function() {
 
-	this.guessesTogether = function() {//need counter
-		newChar.lettersGuessed();
 	}
 	this.callingGuess = function(char) {
 		newChar.checksGuess()
 	}
 }
 
+
 //to test
-var hello = new Word("hello, there");
-hello.guessesTogether();
+// var hello = new Word("hello, there");
+
 
 //would be in index passed through word.
 // var words = "spirited away, howls moving castle, princess mononoke";
